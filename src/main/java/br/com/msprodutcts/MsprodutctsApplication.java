4cc9f7c561e05ffe1502e3c18e63b32d52d0bb67
Bernardo Witkoski <bernardowitkoski@hotmail.com>
2022-09-25 22:36:15 -0300

    Spring initial structure

---
+++ src/main/java/br/com/msprodutcts/MsprodutctsApplication.java
@@ -0,0 +1,13 @@
+package br.com.msprodutcts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsprodutctsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsprodutctsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/msprodutcts/MsprodutctsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.msprodutcts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsprodutctsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
